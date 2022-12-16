65abc75619e95dab3cf1bc5db24fa832439ee27d
Gennady <83316072+1ort@users.noreply.github.com>
2022-12-16 18:26:51 +0500

    Update NewBoard func
    Add board root post

--- db/board.go
+++ db/board.go
@@ -12,5 +12,16 @@ func NewBoard(pool *pgxpool.Pool, slug string, name string, descr string) error 
 	($1, $2, $3)
 	`
 	_, err := pool.Query(context.Background(), new_board_query, slug, name, descr)
-	return err
+	if err != nil {
+		return err
+	}
+	root_post_query := `
+	INSERT INTO post (no, resto, board, com, time) VALUES
+	(0, 0, $1, "", NOW())
+	`
+	_, err = pool.Query(context.Background(), root_post_query, slug, name, descr)
+	if err != nil {
+		return err
+	}
+	return nil
 }
