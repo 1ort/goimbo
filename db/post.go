f84f3e44438a5a1db4c81ee67728bcccc7c86f5c
Gennady <83316072+1ort@users.noreply.github.com>
2022-12-16 18:15:01 +0500

    Post numeration fix

--- db/post.go
+++ db/post.go
@@ -9,7 +9,7 @@ import (
 func NewPost(pool *pgxpool.Pool, resto string, board string, com string) error {
 	query_template := `
 	INSERT INTO post (no, resto, board, com, time) SELECT
-	(COUNT(*), $1, $2, $3, NOW()) FROM post
+	(MAX (no) + 1, $1, $2, $3, NOW()) FROM post
 	WHERE (board=$2)
 	`
 	_, err := pool.Query(context.Background(), query_template, resto, board, com)
