3722c7d83794d5790b75c8221216bc08f5d314c3
Gennady <83316072+1ort@users.noreply.github.com>
2022-12-19 17:47:18 +0500

    Move middlewares into /handler/middleware

---
+++ handler/middleware/middleware.go
@@ -0,0 +1,37 @@
+package middleware
+
+// import (
+// 	"net/http"
+
+// 	"github.com/1ort/goimbo/db"
+// 	"github.com/gin-gonic/gin"
+// 	"github.com/jackc/pgx/v5/pgxpool"
+// )
+
+// func DBConnPool(pool *pgxpool.Pool) gin.HandlerFunc {
+// 	return func(c *gin.Context) {
+// 		c.Set("db", pool)
+// 		c.Next()
+// 	}
+// }
+
+// func IsBoardExists() gin.HandlerFunc {
+// 	return func(c *gin.Context) {
+// 		db_pool := c.MustGet("db").(*pgxpool.Pool)
+// 		board := c.Param("board")
+// 		res, err := db.SelectBoardExists(db_pool, board)
+// 		if err != nil {
+// 			c.AbortWithStatus(http.StatusInternalServerError)
+// 		} else if !res {
+// 			c.AbortWithStatus(http.StatusNotFound)
+// 		} else {
+// 			c.Next()
+// 		}
+
+// 	}
+// }
+
+// func SetupMiddlewares(app *gin.Engine) {
+// 	app.Use(gin.Logger())
+// 	app.Use(gin.Recovery())
+// }
