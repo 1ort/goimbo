c01ac4f1e70e3e329de547049dad10f7ba0f0689
Gennady <83316072+1ort@users.noreply.github.com>
2022-12-21 03:28:26 +0500

    Separate file for api handlers

---
+++ handler/api.go
@@ -0,0 +1,95 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/1ort/goimbo/model"
+	"github.com/gin-gonic/gin"
+)
+
+func (h *Handler) get_boards(c *gin.Context) {
+	ctx := c.Request.Context()
+	boardList, err := h.userspace.Boards(ctx)
+	if err != nil {
+		c.JSON(model.Status(err), gin.H{
+			"status": model.Status(err),
+			"result": err,
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"status": http.StatusOK,
+		"result": boardList,
+	})
+}
+
+func (h *Handler) get_threads(c *gin.Context) {
+	ctx := c.Request.Context()
+	board := c.Param("board")
+	//rawThreadList, err := h.postRepo.GetThreadList(ctx, board)
+	//threadList := append(make([]*model.Post, 0), rawThreadList...)
+	threadList, err := h.userspace.Threads(ctx, board)
+	if err != nil {
+		c.JSON(model.Status(err), gin.H{
+			"status": model.Status(err),
+			"result": err,
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"status": http.StatusOK,
+		"result": threadList,
+	})
+
+}
+
+func (h *Handler) get_catalog(c *gin.Context) {
+	board := c.Param("board")
+	c.JSON(http.StatusOK, gin.H{
+		"status": http.StatusOK,
+		"result": fmt.Sprintf("Catalog of %s board here", board),
+	})
+
+}
+
+func (h *Handler) get_archive(c *gin.Context) {
+	board := c.Param("board")
+	c.JSON(http.StatusOK, gin.H{
+		"status": http.StatusOK,
+		"result": fmt.Sprintf("Archive of %s board here", board),
+	})
+}
+
+func (h *Handler) get_page(c *gin.Context) {
+	board := c.Param("board")
+	page := c.Param("page")
+	if _, err := strconv.Atoi(page); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status": http.StatusBadRequest,
+			"result": fmt.Sprintf("Page number must be an integer, got %s instead", page),
+		})
+	} else {
+		c.JSON(http.StatusOK, gin.H{
+			"status": http.StatusOK,
+			"result": fmt.Sprintf("Page number %s on %s board here", page, board),
+		})
+	}
+}
+
+func (h *Handler) get_thread(c *gin.Context) {
+	board := c.Param("board")
+	op := c.Param("op")
+	if _, err := strconv.Atoi(op); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status": http.StatusBadRequest,
+			"result": fmt.Sprintf("Thread number must be an integer, got %s instead", op),
+		})
+	} else {
+		c.JSON(http.StatusOK, gin.H{
+			"status": http.StatusOK,
+			"result": fmt.Sprintf("Thread number %s content on %s board here", op, board),
+		})
+	}
+}
--- handler/handler.go
+++ handler/handler.go
@@ -1,10 +1,6 @@
 package handler
 
 import (
-	"fmt"
-	"net/http"
-	"strconv"
-
 	"github.com/1ort/goimbo/model"
 	"github.com/gin-gonic/gin"
 )
@@ -27,8 +23,8 @@ func NewHandler(cfg *HandlerConfig) {
 		userspace: cfg.Userspace,
 	}
 	api := cfg.R.Group(cfg.BaseUrl)
-	api.GET("/boards", h.get_boards)
 
+	api.GET("/boards", h.get_boards)
 	board := api.Group("/:board")
 	board.GET("/threads", h.get_threads)
 	board.GET("/catalog", h.get_catalog)
@@ -36,93 +32,4 @@ func NewHandler(cfg *HandlerConfig) {
 	board.GET("/:page", h.get_page)
 	board.GET("/thread/:op", h.get_thread)
 
-	//board.Use(Checkboardmiddleware)
-	//board.POST("/newpost", h.NewPost)
-	//api.POST("/newboard", h.NewBoard)
-
-}
-
-func (h *Handler) get_boards(c *gin.Context) {
-	ctx := c.Request.Context()
-	boardList, err := h.userspace.Boards(ctx)
-	if err != nil {
-		c.JSON(model.Status(err), gin.H{
-			"status": model.Status(err),
-			"result": err,
-		})
-		return
-	}
-	c.JSON(http.StatusOK, gin.H{
-		"status": http.StatusOK,
-		"result": boardList,
-	})
-}
-
-func (h *Handler) get_threads(c *gin.Context) {
-	ctx := c.Request.Context()
-	board := c.Param("board")
-	//rawThreadList, err := h.postRepo.GetThreadList(ctx, board)
-	//threadList := append(make([]*model.Post, 0), rawThreadList...)
-	threadList, err := h.userspace.Threads(ctx, board)
-	if err != nil {
-		c.JSON(model.Status(err), gin.H{
-			"status": model.Status(err),
-			"result": err,
-		})
-		return
-	}
-	c.JSON(http.StatusOK, gin.H{
-		"status": http.StatusOK,
-		"result": threadList,
-	})
-
-}
-
-func (h *Handler) get_catalog(c *gin.Context) {
-	board := c.Param("board")
-	c.JSON(http.StatusOK, gin.H{
-		"status": http.StatusOK,
-		"result": fmt.Sprintf("Catalog of %s board here", board),
-	})
-
-}
-
-func (h *Handler) get_archive(c *gin.Context) {
-	board := c.Param("board")
-	c.JSON(http.StatusOK, gin.H{
-		"status": http.StatusOK,
-		"result": fmt.Sprintf("Archive of %s board here", board),
-	})
-}
-
-func (h *Handler) get_page(c *gin.Context) {
-	board := c.Param("board")
-	page := c.Param("page")
-	if _, err := strconv.Atoi(page); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"status": http.StatusBadRequest,
-			"result": fmt.Sprintf("Page number must be an integer, got %s instead", page),
-		})
-	} else {
-		c.JSON(http.StatusOK, gin.H{
-			"status": http.StatusOK,
-			"result": fmt.Sprintf("Page number %s on %s board here", page, board),
-		})
-	}
-}
-
-func (h *Handler) get_thread(c *gin.Context) {
-	board := c.Param("board")
-	op := c.Param("op")
-	if _, err := strconv.Atoi(op); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"status": http.StatusBadRequest,
-			"result": fmt.Sprintf("Thread number must be an integer, got %s instead", op),
-		})
-	} else {
-		c.JSON(http.StatusOK, gin.H{
-			"status": http.StatusOK,
-			"result": fmt.Sprintf("Thread number %s content on %s board here", op, board),
-		})
-	}
 }
