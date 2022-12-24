0d3c00a3644adca28e46891bb3f91abf02410aa5
Gennady <83316072+1ort@users.noreply.github.com>
2022-12-24 06:37:58 +0500

    Update pg_post_repository.go

--- repository/pg_post_repository.go
+++ repository/pg_post_repository.go
@@ -1,23 +1,26 @@
 package repository
 
-import ()
+import (
+	"context"
 
+	"github.com/1ort/goimbo/model"
+)
 
 type PgPostRepository struct {
 }
 
 type PgPostRepoConfig struct {
-	connection
+	//connection
 }
 
 func (p *PgPostRepository) NewPost(ctx context.Context, board, com string, parent int) error {
 	return nil
 }
 
-func (p *PgPostRepository) GetSingle(ctx context.Context, board string, no int) (*Post, error) {
-	return (nil, nil)
+func (p *PgPostRepository) GetSingle(ctx context.Context, board string, no int) (*model.Post, error) {
+	return nil, nil
 }
 
-func (p *PgPostRepository) GetMultiple(ctx context.Context, board string, parent int, skip, limit int) ([]*Post, error) {
-	return (nil, nil)
+func (p *PgPostRepository) GetMultiple(ctx context.Context, board string, parent int, skip, limit int) ([]*model.Post, error) {
+	return nil, nil
 }
